b7b6cba8e014a78baf0c6ea16cc4dd68132d4149
phat79186 <phat79186@gmail.com>
2026-07-22 18:53:57 +0700

    Update mod version to 26.1 in mods.toml and build scripts

--- update_combined_jar.py
+++ update_combined_jar.py
@@ -1,4 +1,4 @@
-import zipfile, os
+import zipfile, os, shutil
 
 combined_mods_toml = """modLoader="javafml"
 loaderVersion="[47,)"
@@ -6,7 +6,7 @@ license="GNU GPLv3"
 
 [[mods]]
 modId="reignofnether"
-version="1.3.6b"
+version="26.1"
 displayName="Reign of Nether"
 logoFile="examplemod.png"
 credits="Thanks to Technovision for his YouTube guides"
@@ -31,7 +31,7 @@ Minecraft, except it's an RTS
 
 [[mods]]
 modId="ron_golem_healer_integration"
-version="1.0.0"
+version="26.1"
 displayName="RoN Golem Healer Integration"
 authors="Codex"
 description='''Integrates the original Villager Golem Healer entity as a Reign of Nether unit.'''
@@ -52,14 +52,18 @@ side="BOTH"
 """
 
 codex_files = {}
-for root, dirs, files in os.walk('c:/Users/User/Desktop/New folder (7)/com/codex'):
+base_dir = os.path.dirname(os.path.abspath(__file__))
+com_codex_dir = os.path.join(base_dir, 'com', 'codex')
+
+for root, dirs, files in os.walk(com_codex_dir):
     for f in files:
         full = os.path.join(root, f)
-        rel = os.path.relpath(full, 'c:/Users/User/Desktop/New folder (7)')
+        rel = os.path.relpath(full, base_dir)
         rel_zip = rel.replace('\\', '/')
         codex_files[rel_zip] = open(full, 'rb').read()
 
-mixin_json_data = open('c:/Users/User/Desktop/New folder (7)/ron_golem_healer_integration.mixins.json', 'rb').read()
+mixin_json_path = os.path.join(base_dir, 'ron_golem_healer_integration.mixins.json')
+mixin_json_data = open(mixin_json_path, 'rb').read()
 
 def update_jar(jar_path):
     if not os.path.exists(jar_path):
@@ -94,19 +98,37 @@ def update_jar(jar_path):
         os.replace(temp_path, jar_path)
         print(f'Successfully updated combined jar: {jar_path}')
     except PermissionError:
-        print(f'Warning: Could not overwrite {jar_path} because it is currently in use (Minecraft might be running).')
+        print(f'Warning: Could not overwrite {jar_path} because it is currently in use.')
         if os.path.exists(temp_path):
             os.remove(temp_path)
 
-update_jar('c:/Users/User/Desktop/New folder (7)/reignofnether-overhaul-1.0.1.jar')
-update_jar('c:/Users/User/Desktop/New folder (7)/reignofnether-1.3.6b.jar')
-update_jar('c:/Users/User/Desktop/New folder (7)/reignofnether_base.jar')
-update_jar('C:/Users/User/AppData/Roaming/.minecraft/mods/reignofnether-1.3.6b.jar')
-if os.path.exists('C:/Users/User/AppData/Roaming/.minecraft/mods/reignofnether-overhaul-1.0.1.jar'):
-    update_jar('C:/Users/User/AppData/Roaming/.minecraft/mods/reignofnether-overhaul-1.0.1.jar')
-try:
-    import shutil
-    shutil.copy('c:/Users/User/Desktop/New folder (7)/reignofnether-overhaul-1.0.1.jar', 'C:/Users/User/AppData/Roaming/.minecraft/mods/')
-    print('Copied reignofnether-overhaul-1.0.1.jar to .minecraft/mods')
-except Exception as e:
-    print(f'Note: Could not copy jar to .minecraft/mods: {e}')
+# Prepare 26.1 jar based on base jar or existing jar
+base_jar = os.path.join(base_dir, 'reignofnether_base.jar')
+target_261_jar = os.path.join(base_dir, 'reignofnether-overhaul-26.1.jar')
+target_101_jar = os.path.join(base_dir, 'reignofnether-overhaul-1.0.1.jar')
+
+if os.path.exists(base_jar):
+    shutil.copy(base_jar, target_261_jar)
+    shutil.copy(base_jar, target_101_jar)
+
+update_jar(target_261_jar)
+update_jar(target_101_jar)
+update_jar(base_jar)
+
+mc_mods_dir = 'C:/Users/User/AppData/Roaming/.minecraft/mods'
+mc_261 = os.path.join(mc_mods_dir, 'reignofnether-overhaul-26.1.jar')
+mc_101 = os.path.join(mc_mods_dir, 'reignofnether-overhaul-1.0.1.jar')
+
+if os.path.exists(mc_mods_dir):
+    try:
+        shutil.copy(target_261_jar, mc_261)
+        print(f'Copied reignofnether-overhaul-26.1.jar to {mc_mods_dir}')
+    except Exception as e:
+        print(f'Note: Could not copy 26.1 jar: {e}')
+
+    try:
+        if os.path.exists(mc_101):
+            os.remove(mc_101)
+            print(f'Removed old reignofnether-overhaul-1.0.1.jar from {mc_mods_dir}')
+    except Exception as e:
+        print(f'Note: Could not remove old 1.0.1 jar: {e}')
